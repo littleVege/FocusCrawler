7b32b6f4600664402cf02ed2ad28559194be21a6
littleVege <caidroid713@gmail.com>
2015-01-06 00:35:19 +0800

    init

---
+++ FocusCrawler/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FocusCrawler
+{
+    class Program
+    {
+        private static Queue<string> _urlQueue = new Queue<string>();
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Download(string url)
+        {
+            var client = new WebClient();
+            var html = "";
+            var retry = 0;
+            var success = false;
+            do
+            {
+                try
+                {
+                    html = client.DownloadString(url);
+                    success = true;
+                }
+                catch (WebException e)
+                {
+                    switch (e.Status)
+                    {
+                        case WebExceptionStatus.Timeout:
+                        case WebExceptionStatus.SendFailure:
+                        case WebExceptionStatus.ReceiveFailure:
+                        case WebExceptionStatus.ConnectionClosed:
+                            retry++;
+                            break;
+                        case WebExceptionStatus.ProtocolError:
+                            success = true;
+                            break;
+                        default:
+                            _urlQueue.Enqueue(url);
+                            success = true;
+                            break;
+                    }
+                    /*TODO:log this Error and time*/
+                }
+            } while (retry <= 3 || success);
+
+        }
+
+        static void ResolveItem(string url,string htmlStr)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlStr);
+            var itemDic = new Dictionary<string, object>();
+            var document = htmlDoc.DocumentNode;
+            var cid = new Regex("\\d+",RegexOptions.ECMAScript).Match(url).ToString();
+            var title = document.SelectSingleNode("//*[@class='f_yahei f26 p_name']").InnerText;
+            var ranks = document.SelectNodes("//*[@class='f36 f_arial c136']");
+            var assortRank = ranks[0].InnerText;
+            var trifficRank = ranks[1].InnerText;
+            var region = document.SelectSingleNode("//dl[@class='r_rank']/dt").InnerText;
+            var price = document.SelectSingleNode("//b[@class='f_arial f24']").InnerText;
+            var assortRate = document.SelectSingleNode("//i[@class='fb f18 f_arial']").InnerText;
+            assortRate = new Regex("\\d+").Match(assortRate).ToString();
+            itemDic.Add("cid",int.Parse(cid));
+            itemDic.Add("url",url);
+            itemDic.Add("title",title);
+            itemDic.Add("assort_rank",int.Parse(assortRank));
+            itemDic.Add("triffic_rank",int.Parse(trifficRank));
+            itemDic.Add("region",region);
+            itemDic.Add("price",double.Parse(price));
+            itemDic.Add("assort_rate",double.Parse(assortRate)/100);
+
+            var scripts = document.SelectNodes("//script");
+            foreach (HtmlNode script in scripts)
+            {
+                var inner = script.InnerText;
+                if (new Regex("parkAssort[\\s]*=[\\s]*([^;])+;",RegexOptions.ECMAScript).IsMatch(inner))
+                {
+                    
+                }
+            }
+        }
+
+        static List<Dictionary<string, object>> ParseAssorts(string script)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var matched = new Regex("parkAssort[\\s]*=[\\s]*([^;])+;", RegexOptions.ECMAScript).Matches(script);
+            if (matched.Count < 0)
+            {
+                return null;
+            }
+            var jsonStr = matched[1].ToString();
+            var parkAssort = JObject.Parse(jsonStr);
+            var assorts = (JArray)parkAssort["assort_15m"];
+
+            return results;
+        }
+    }
+}
---
+++ FocusCrawler/sql/focus_house.sql
@@ -0,0 +1,99 @@
+﻿-- Database: focus_house
+
+-- DROP DATABASE focus_house;
+
+CREATE DATABASE focus_house
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
+
+-- Table: comms
+
+-- DROP TABLE comms;
+
+CREATE TABLE comms
+(
+  cid integer NOT NULL,
+  url text,
+  built_at date,
+  property_fee double precision,
+  houses integer,
+  parking integer,
+  green_rate double precision,
+  plot_ratio integer,
+  region character varying(255),
+  region_rank integer,
+  traffic_rank integer,
+  pm character varying(255),
+  red character varying(255),
+  assort_rate double precision
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE comms
+  OWNER TO postgres;
+
+-- Table: assorts
+
+-- DROP TABLE assorts;
+
+CREATE TABLE assorts
+(
+  cid integer NOT NULL,
+  assort_id integer NOT NULL,
+  distance integer,
+  cost_time integer,
+  type_id integer,
+  lat double precision,
+  lon double precision,
+  assort_type_id integer,
+  address character varying(255),
+  price double precision,
+  CONSTRAINT assorts_pkey PRIMARY KEY (cid, assort_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE assorts
+  OWNER TO postgres;
+
+
+-- Table: history_prices
+
+-- DROP TABLE history_prices;
+
+CREATE TABLE history_prices
+(
+  cid integer NOT NULL,
+  datetime date NOT NULL,
+  price double precision,
+  CONSTRAINT history_prices_pkey PRIMARY KEY (cid, datetime)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE history_prices
+  OWNER TO postgres;
+
+
+-- Table: raw_pages
+
+-- DROP TABLE raw_pages;
+
+CREATE TABLE raw_pages
+(
+  url text,
+  crawl_time timestamp without time zone NOT NULL DEFAULT now(),
+  response text NOT NULL,
+  id bigserial NOT NULL,
+  CONSTRAINT raw_pages_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE raw_pages
+  OWNER TO postgres;
